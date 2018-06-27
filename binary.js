3110537b35be39abf3128df1b731c1de91e141b5
Abshir <abshirm@live.com>
2018-06-27 15:10:50 -0700

    initial commit

---
+++ binary.js
@@ -0,0 +1,24 @@
+// Abshir Mohamed
+// CS120
+// Convert numbers to their respective base numbers
+// 4/11/2018
+
+// A. 100111 base 2
+//B. 1101001 base 2
+//C. 115 base 8
+//D.177 base 8
+//E.4F base 16
+//F. 1A9 base 16
+//G. 53 base 10
+//H.169 base 10
+//I. 26 base 10
+//J. 157 base 10
+//K. 245 base 10
+//L. 41394 base 10
+//+++++++++++++++++++++++++++++++//
+
+//Part 2//
+
+//W h a t  i s  two + 5 ?
+//Answer = 55
+
