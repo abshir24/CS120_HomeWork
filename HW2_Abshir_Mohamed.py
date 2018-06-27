c8b7c93ab56d889e2062077881e30ef6511e2a63
Abshir <abshirm@live.com>
2018-06-27 15:13:11 -0700

    hw2

---
+++ HW2_Abshir_Mohamed.py
@@ -0,0 +1,75 @@
+# Homework 2
+# cs120
+# Abshir Mohamed
+# This is a program that displays a little bit about myself the program also uses print statements 
+# as well as addition and subtraction operators to print mathematical statements to the console
+# 4/12/2018
+
+# print "Name: Abshir"
+
+# print "Favorite musician: Stevie Wonder"
+
+# print "Favorite Food: sukuma wiki"
+
+
+# Pt1
+
+# ------------------------------------------------
+
+# 256 base 10
+# 171 base 10
+# 1101101 base 2
+# C5 base 16
+
+# ------------------------------------------------
+
+# PT2
+
+# ------------------------------------------------
+
+
+# print "What can I do in python?"
+# print ()
+# print "What is the value of 2+4+6/3"
+# print (2+4+6/3)
+# print ("Why:The cpu follows the order of operations so first it divides 6by3 and then adds 2+4 after")
+# print "What is the value of 24+30/6"
+# print (24+30/6)
+# print ("Why:The cpu follows the order of operations so first it divides 30by6 and then adds 24+5 after")
+# print "What is the value of 3 + 5 < 5-7"
+# print (3+5<5-7)
+# print "Why: this returns a false boolean because 8 is not less than -2"
+# print "What is the value of 3**4?"
+# print (3**4)
+# print "Why: Because 3^4 = " + str((3**4))
+# print "What is the value of 5 <= 45/9"
+# print (5 <= 45/9)
+# print "Why: this returns a true boolean because 45/8 is == 5"
+
+# -----------------------------------------------------
+
+
+#Pt3
+
+# -----------------------------------------------------
+
+# meal1 = 44
+# meal2 = 30
+# meal3 = 12
+# meal4 = 37
+
+# subtotal = meal1+meal2+meal3+meal4
+# tax = subtotal * .05
+# tip = subtotal * .15
+# total = subtotal+tax+tip
+
+# print "The subtotal was : $",subtotal
+# print "The tax was : $",tax
+# print "The tip was : $",tip
+# print "The total was : $",total
+
+# ------------------------------------------------------
+
+
+
+
